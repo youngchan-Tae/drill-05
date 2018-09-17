3a952d5c3dc5ba4ded363256d3deac8c7e83ec70
youngchan-Tae <1140116@naver.com>
2018-09-17 17:06:41 +0900

    2015182043
    뼈대와 첫 움직임 구현

--- drill-05.py
+++ drill-05.py
@@ -1,81 +1,61 @@
 from pico2d import *
-import math
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x,y)
-        x = x + 2
-        delay(0.01)
+def move_direct1():
+    x, y = 203, 535
+    x1, y1 = 203, 535
+    x2, y2 = 132, 243
+    count = 0
 
+    move_x1 = x2 - x1 // 10
+    move_y1 = y2 - y1 // 10
 
-def move_up():
-    x, y = 800 - 25, 90
-    while y < 600 - 50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x,y)
-        y = y + 2
-        delay(0.01)
-
-def move_left():
-    x, y = 800 - 25, 600-50
-    while x > 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x,y)
-        x = x - 2
-        delay(0.01)
-
-def move_down():
-    x, y = 25, 600-50
-    while y > 90:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x,y)
-        y = y - 2
-        delay(0.01)
-
-def move_from_left_to_center():
-    x, y = 25, 90
-    while x < 800 // 2:
+    while count < 10:
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        x = x + 2
-        delay(0.01)
+        x = x + 7.1
+        y = y + 29.2
+        count = count + 1
+        delay(0.1)
 
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
+def move_direct2():
+    pass
 
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, (600-100) // 2
+def move_direct3():
+    pass
 
-    degree = -90
-    while degree < 270:
-        radian = math.radians(degree)
-        x = cx + r * math.cos(radian)
-        y = cy + r * math.sin(radian)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        degree = degree + 1
-        delay(0.01)
+def move_direct4():
+    pass
 
+def move_direct5():
+    pass
 
+def move_direct6():
+    pass
+
+def move_direct7():
+    pass
+
+def move_direct8():
+    pass
+
+def move_direct9():
+    pass
 
 while True:
-    make_rectangle()
-    make_circle()
+    move_direct1()
+    move_direct2()
+    move_direct3()
+    move_direct4()
+    move_direct5()
+    move_direct6()
+    move_direct7()
+    move_direct8()
+    move_direct9()
 
+    
 close_canvas()
