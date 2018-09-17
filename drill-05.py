5a09268c6923881c09633cf82ac39d13098a8efe
youngchan-Tae <1140116@naver.com>
2018-09-17 16:34:13 +0900

    2015182043
    사각형 움직임 구현 완료

--- drill-05.py
+++ drill-05.py
@@ -9,23 +9,47 @@ def move_from_center_to_right():
     x, y = 800 // 2, 90
     while x < 800 - 25:
         clear_canvas_now()
-        grass.draw.now(400, 30)
+        grass.draw_now(400, 30)
         character.draw_now(x,y)
         x = x + 2
         delay(0.01)
 
 
 def move_up():
-    pass
+    x, y = 800 - 25, 90
+    while y < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        y = y + 2
+        delay(0.01)
 
 def move_left():
-    pass
+    x, y = 800 - 25, 600-50
+    while x > 25:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        x = x - 2
+        delay(0.01)
 
 def move_down():
-    pass
+    x, y = 25, 600-50
+    while y > 90:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        y = y - 2
+        delay(0.01)
 
 def move_from_left_to_center():
-    pass
+    x, y = 25, 90
+    while x < 800 // 2:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
 
 def make_rectangle():
     move_from_center_to_right()
@@ -33,10 +57,10 @@ def make_rectangle():
     move_left()
     move_down()
     move_from_left_to_center()
-    pass
 
 def make_circle():
-    pass
+    
+
 
 
 while True:
